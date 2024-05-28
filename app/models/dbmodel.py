51a9a4acc8c5ff32c0b5d1757c1e74fbf5e5021c
Li Jinliang <necerlixin@outlook.com>
2024-05-28 16:40:37 +0800

    修改数据库模型--by李锦亮

--- app/models/dbmodel.py
+++ app/models/dbmodel.py
@@ -21,7 +21,7 @@ class News(db.Model):
     news_title = db.Column(db.String(32))
     news_author = db.Column(db.String(32))
     news_date = db.Column(db.String(32))
-    news_content = db.Column()
+    news_content = db.Column(db.Text)
 
 class User(db.Model):
     # 用户表
