fb7adf586d5dc938b36b1a5e75717f87b57d0a1e
Li Jinliang <necerlixin@outlook.com>
2024-05-29 09:45:04 +0800

    api测试完成--by李锦亮

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -23,9 +23,10 @@ def get_brief(content:list,length=60)->str:
     text_list = [sample['data'] for sample in content if sample['type']=="text"]
     text = "".join(text_list)
     if len(text) > length:
-        return text[:length]
+        return text[:length] + "……"
     else:
-        return text
+        return text + "……"
+    
     
     
 
@@ -152,7 +153,7 @@ def create_blueprint_users():
         account = request.form.get('account')
         password = request.form.get('password')
         resp = make_response()
-        resp.headers['Content-Type'] = 'application/json; charset=UTF-8'
+        
         
         if account and password is not None:
             user = User.query.filter_by(user_account=account).first()
@@ -161,20 +162,20 @@ def create_blueprint_users():
                 if password == user_password:
                     resp.set_data(jsonify(msg='登陆成功').get_data())
                     resp.status_code = StatusCode.CODE_FINISTH
-                    return resp
                 else:
-                    resp.set_data(jsonify(mgs="账号错误").get_data())
+                    resp.set_data(jsonify(mgs="密码错误").get_data())
                     resp.status_code = StatusCode.CODE_UNDERSTAND_REFUSE
                     # 理解客户段请求，但是拒绝
-                    return resp
 
             else :
                 resp = make_response(jsonify(msg="用户不存在").get_data())
                 resp.status_code = StatusCode.CODE_CANT_FINISHT
                 # 服务器无法根据客户端请求的内容特性完成请求
-                return resp
+            resp.headers['Content-Type'] = 'application/json; charset=UTF-8'
+            return resp
+            
     
-    @bp.route("/user/addhistory",methods=["POST"])
+    @bp.route("/users/addhistory",methods=["POST"])
     def add_history():
         user_account = request.form.get('account')
         news_id = request.form.get('news_id')
@@ -189,13 +190,13 @@ def create_blueprint_users():
                                   history_time=time)
                 db.session.add(history)
                 db.session.commit()
-                resp.set_data(str({'msg':"历史记录创建成功"}))
+                resp.set_data(jsonify(msg="历史记录创建成功").get_data())
                 resp.status_code = StatusCode.CODE_FINISTH
             else:
-                resp.set_data(str({'msg':"用户不存在"}))
+                resp.set_data(jsonify(msg="用户不存在").get_data())
                 resp.status_code = StatusCode.CODE_FINISTH
         else:
-            resp.set_data(str({'msg':"请求格式错误"}))
+            resp.set_data(jsonify(msg="请求格式错误").get_data())
             resp.status_code = StatusCode.CODE_SYTAX_ERROR
         resp.headers['Content-Type'] = "application/json; charset=UTF-8"
         return resp
@@ -207,26 +208,28 @@ def create_blueprint_users():
         resp = make_response()
         if history_list:
             history_news_id = [item.news_id for item in history_list]
-            case_statement = db.case(
-                [(News.news_id == news_id, index) for index, news_id in enumerate(history_news_id)],
-                else_=len(history_news_id)
-            )
-            news_list = News.query.filter(News.news_id.in_(history_news_id)).order_by(case_statement).all()
+            # case_statement = db.case(
+            #     *[(News.news_id == news_id, index) for index, news_id in enumerate(history_news_id)],
+            #     else_=len(history_news_id)
+            # )
+            # news_list = News.query.filter(News.news_id.in_(history_news_id)).order_by(case_statement).all()
             history_data  = []
             for i in range(len(history_list)):
-                brief = get_brief(news_list[i].content)
+                news = News.query.filter_by(news_id=history_news_id[i]).first()
+                content = json.loads(news.news_content)
+                brief = get_brief(content)
                 sample = {
                           "history_time":history_list[i].history_time,
-                          "title":news_list[i].news_title,
-                          "img_url":news_list[i].new_img_url,
+                          "title":news.news_title,
+                          "img_url":news.new_img_url,
                           "news_brief":brief,
-                          "news_id":news_list[i].news_id
+                          "news_id":news.news_id
                           }
                 history_data.append(sample)
-            resp.set_data(str(history_data))
+            resp.set_data(jsonify(history_data).get_data())
             resp.status_code = StatusCode.CODE_FINISTH
         else:
-            resp.set_data(str("无历史记录"))
+            resp.set_data(jsonify(msg="无历史记录").get_data())
             resp.status_code = StatusCode.CODE_UNDERSTAND_REFUSE
         resp.headers['Content-Type'] = "application/json; charset=UTF-8"
         return resp
